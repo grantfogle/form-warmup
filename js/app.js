2ee6c3c6cf891d9f4fdeb6c1f5ada327227ebe13
justcallmebeef <bethanymitch10@gmail.com>
2018-09-21 11:24:17 -0600

    story 2 finished

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var amount = document.querySelector(".amount")
+var form = document.querySelector("form")
+var tipMessage = document.querySelector(".tip-message")
+
+form.addEventListener("submit", calculateTip)
+
+function calculateTip(event) {
+    event.preventDefault();
+    console.log(amount.value)
+    var result = amount.value * .2
+    tipMessage.innerHTML = "You should tip $" + result.toFixed(2)
+}
