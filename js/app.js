37922d1895c0758f256bd771eabbc696cd8fbdf9
grantfogle <grantjfogle@gmail.com>
2018-09-24 09:38:40 -0600

    refactored code complete...

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
-var amount = document.querySelector(".amount")
-var form = document.querySelector("form")
-var tipMessage = document.querySelector(".tip-message")
-var option = document.getElementsByTagName("option")
+var amount = document.querySelector(".amount");
+var form = document.querySelector("form");
+var tipMessage = document.querySelector(".tip-message");
+var option = document.getElementsByTagName("option");
 var select = document.querySelector("select");
-var tip = 0
+var tip = 0;
 
-console.log(option[1].innerHTML)
+console.log(option[1].innerHTML);
 
-form.addEventListener("submit", calculateTip)
+form.addEventListener("submit", calculateTip);
 
 function calculateTip(event) {
+    var msg = "You should tip $";
     event.preventDefault();
-    console.log(tip)
     var result = amount.value * tip;
-    tipMessage.innerHTML = "You should tip $" + result.toFixed(2)
-    tipMessage.classList.add("bg-info")
+    tipMessage.innerHTML = msg + result.toFixed(2);
+    tipMessage.classList.add("bg-info");
 }
 
-select.addEventListener("change", getTipValue)
+select.addEventListener("change", getTipValue);
 
 function getTipValue(event) {
     for (var i = 0; i < option.length; i++) {
         if (option[i].selected) {
-            console.log(option[i].innerHTML)
-            return tip = option[i].value
+            return tip = option[i].value;
         }
     }
 }
